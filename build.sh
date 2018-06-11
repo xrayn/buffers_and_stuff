3c3c456038883ed8a4302b58d57ffce7ed874a5f
Andre Rein <andre.rein@huawei.com>
2018-06-11 11:40:09 +0200

    added clean script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p output
 gcc -g -fstack-protector stack_variables/stack4_dop_with_canary.c -o output/stack4_dop_with_canary
 gcc -g stack_variables/stack.c -o output/stack
 gcc -g -fstack-protector stack_variables/stack.c -o output/stack_canary
